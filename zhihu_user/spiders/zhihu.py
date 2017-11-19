f55074584816cdd6297762060f2a9690e4a82743
jjunxi <jjunxi417@gmail.com>
2017-11-19 23:34:21 +0800

    url format

---
+++ zhihu_user/spiders/zhihu.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider, Request
+
+class ZhihuSpider(Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+
+    start_user = 'jjunxi'
+
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query_include = 'locations,employments,gender,educations,business,voteup_count,thanked_Count,follower_count,following_count,cover_url,following_topic_count,following_question_count,following_favlists_count,following_columns_count,avatar_hue,answer_count,articles_count,pins_count,question_count,columns_count,commercial_question_count,favorite_count,favorited_count,logs_count,included_answers_count,included_articles_count,included_text,message_thread_token,account_status,is_active,is_bind_phone,is_force_renamed,is_bind_sina,is_privacy_protected,sina_weibo_url,sina_weibo_name,show_sina_weibo,is_blocking,is_blocked,is_following,is_followed,is_org_createpin_white_user,mutual_followees_count,vote_to_count,vote_from_count,thank_to_count,thank_from_count,thanked_count,description,hosted_live_count,participated_live_count,allow_message,industry_category,org_name,org_homepage,badge[?(type=best_answerer)].topics'
+
+    followees_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
+    followees_query_include = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+
+    def start_requests(self):
+        url = self.user_url.format(user=self.start_user, include = self.user_query_include)
+        yield Request(url, callback=self.parse_user)
+
+
+
+    def parse_user(self, response):
+        print(response.text)
